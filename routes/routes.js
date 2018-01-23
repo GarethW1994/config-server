5b0529c08b27d772fe29ad1728e387271eaf4448
Gareth <williams.gareth0@gmail.com>
2018-01-23 10:27:03 +0200

    modified the update config route

--- routes/routes.js
+++ routes/routes.js
@@ -32,30 +32,38 @@ module.exports = function(models) {
     const writeconfig = function(req, res, next) {
         // write config data to mongodb
         var conf = req.body;
-        models.myconfig.create(conf, function (err, results) {
-            if (err){
-                if(err.code ==11000){
-                    models.myconfig.findOne({
-                        adminAccount: conf.adminAccount
-                    }, function(err, updatedresults){
-                        if(err){
-                            return next(err);
-                        }else{
-                            updatedresults.conf = conf;
-                            updatedresults.save();
-                            console.log(updatedresults)
-                            res.redirect("/readconfig")
-                        }
-                    })
-                }
+        // models.myconfig.create(conf, function (err, results) {
+        //     if (err){
+        //         if(err.code ==11000){
+        //             models.myconfig.findOne({
+        //                 adminAccount: conf.adminAccount
+        //             }, function(err, updatedresults){
+        //                 if(err){
+        //                     return next(err);
+        //                 }else{
+        //                     updatedresults.conf = conf;
+        //                     updatedresults.save();
+        //                     console.log(updatedresults)
+        //                     res.redirect("/readconfig")
+        //                 }
+        //             })
+        //         }
               
-            }
-            //console.log(err);
-            //console.log(results);
+        //     }
+        //     //console.log(err);
+        //     //console.log(results);
   
             
   
-          })
+        //   })
+
+        models.myconfig.update({
+			adminAccount: conf.adminAccount
+		}, {
+			modules: conf.modules
+		}, function (options, callback) {
+			console.log(callback);
+        });
         // res.send('writeconfig');
         // res.json(results);
     }
