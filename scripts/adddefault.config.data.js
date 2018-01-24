908fc87ccd7ca115bd46401c6d307df29116f5ae
Gareth <williams.gareth0@gmail.com>
2018-01-24 11:03:03 +0200

    updated adding default config data

--- scripts/adddefault.config.data.js
+++ scripts/adddefault.config.data.js
@@ -5,7 +5,7 @@ const Models = require('../models');
 const mongoURL = process.env.MONGO_DB_URL || 'mongodb://localhost/config-server';
 const models = Models(mongoURL);
 
-const defualt_config = function () {
+async function importConfigData() {
 
     // const filePath = ''
     fs.readFile('./config.json', 'utf8', function (err, data) {
